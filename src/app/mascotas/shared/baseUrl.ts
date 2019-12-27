0909f7d92c04612befaea4199b9c01aae36e8607
francomercado93 <mercadofranco.a@gmail.com>
2019-12-27 16:03:07 -0300

    Fix de baseUrl

--- src/app/mascotas/shared/baseUrl.ts
+++ src/app/mascotas/shared/baseUrl.ts
@@ -1,3 +1,5 @@
 export const baseUrl = 'http://localhost:3000/api'
 
 export const baseUrlApiLocal = 'http://localhost:8090/'
+
+export const baseUrlHeroku = 'https://refugio-java.herokuapp.com/api/'
