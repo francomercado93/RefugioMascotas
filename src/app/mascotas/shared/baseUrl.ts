99c53f8212e3587cf6e0137748d6b9e946e52c61
francomercado93 <mercadofranco.a@gmail.com>
2019-12-27 17:24:21 -0300

    Comentario

--- src/app/mascotas/shared/baseUrl.ts
+++ src/app/mascotas/shared/baseUrl.ts
@@ -2,4 +2,4 @@ export const baseUrl = 'http://localhost:8080/api'
 
 export const baseUrlApiLocal = 'http://localhost:8090/'
 
-export const baseUrlHeroku = 'https://refugio-java.herokuapp.com/api/'
+export const baseUrlHeroku = 'https://refugio-java.herokuapp.com/api/'// heroku
